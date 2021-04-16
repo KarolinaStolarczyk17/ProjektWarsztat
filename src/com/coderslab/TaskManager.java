dc2479dd70b263484394df55dbb259e300a9852c
Karolina Stolarczyk <stolarczykkarolina@gmail.com>
2021-04-16 11:12:25 +0200

    projekt

--- src/com/coderslab/TaskManager.java
+++
@@ -1,8 +0,0 @@
-package com.coderslab;
-
-public class TaskManager {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/pl/coderslab/TaskManager.java
@@ -0,0 +1,29 @@
+package pl.coderslab;
+
+public class TaskManager {
+
+    public static void main(String[] args) {
+
+        static final String Nazwa = "tasks.csv";
+        static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+
+        static String[][] tasks;
+
+        public static void printOptions (String[] tab){
+            System.out.println(ConsoleColors.BLUE);
+            System.out.println("Please select an option: " + ConsoleColors.RESET);
+            for (String option : tab) {
+                System.out.println(option);
+
+                public static void main (String[]args){
+                    printOptions(OPTIONS);
+                }
+
+
+            }
+        }
+
+
+        // write your code here
+    }
+}
