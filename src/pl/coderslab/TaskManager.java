b3faec58c09f931309e87304ca6d3078b0097bf4
Karolina Stolarczyk <stolarczykkarolina@gmail.com>
2021-04-17 19:48:12 +0200

    Projekt

--- src/pl/coderslab/TaskManager.java
+++ src/pl/coderslab/TaskManager.java
@@ -1,29 +1,61 @@
 package pl.coderslab;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class TaskManager {
+    static final String FILE_NAME = "tasks.csv";
+    static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+
+    static String[][] tasks;
 
     public static void main(String[] args) {
+        tasks = loadDataToTab(FILE_NAME);
+        printOptions(OPTIONS);
 
-        static final String Nazwa = "tasks.csv";
-        static final String[] OPTIONS = {"add", "remove", "list", "exit"};
+    }
 
-        static String[][] tasks;
+    public static void printOptions(String[] tab) {
+        System.out.println(ConsoleColors.BLUE);
+        System.out.println("Please select an option: " + ConsoleColors.RESET);
+        for (String option : tab) {
+            System.out.println(option);
 
-        public static void printOptions (String[] tab){
-            System.out.println(ConsoleColors.BLUE);
-            System.out.println("Please select an option: " + ConsoleColors.RESET);
-            for (String option : tab) {
-                System.out.println(option);
-
-                public static void main (String[]args){
-                    printOptions(OPTIONS);
-                }
-
-
+        }
+    }
+    public static String[][] loadDataToTab(String fileName){
+        Path dir = Paths.get(fileName);
+        if (!Files.exists(dir)) {
+            System.out.println("File not exist.");
+            System.exit(0);
+        }
+        String[][] tab = null;
+        List<String> strings = null;
+        try {
+            strings = Files.readAllLines(dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tab = new String[strings.size()][strings.get(0).split(",").length];
+        for (int i = 0; i < strings.size(); i++) {
+            String[] split = strings.get(i).split(",");
+            for (int j = 0; j < split.length; j++) {
+                tab[i][j] = split[j];
             }
         }
+        return tab;
 
 
-        // write your code here
+
+
     }
+
+
+
+
+    // write your code here
 }
+
